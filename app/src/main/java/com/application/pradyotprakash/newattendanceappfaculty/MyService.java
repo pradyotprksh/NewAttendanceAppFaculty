a3ea3e4569807efdf0b26525adfefb2fda6dc096
pradyot <pradyotprksh4@gmail.com>
2018-05-01 00:00:36 +0530

    Notification Done

---
+++ app/src/main/java/com/application/pradyotprakash/newattendanceappfaculty/MyService.java
@@ -0,0 +1,40 @@
+package com.application.pradyotprakash.newattendanceappfaculty;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onTaskRemoved(intent);
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startForeground(1,new Notification());
+    }
+
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Intent restartServiceIntent = new Intent(getApplicationContext(), this.getClass());
+        restartServiceIntent.setPackage(getPackageName());
+        startService(restartServiceIntent);
+        super.onTaskRemoved(rootIntent);
+    }
+}
